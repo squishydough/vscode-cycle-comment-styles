f2f7be242b626cb212b28c2d60799c087b9e9941
joshwaiam <joshwaiam@gmail.com>
2022-04-15 00:32:27 -0400

    Refactored a bit

--- src/extension.ts
+++ src/extension.ts
@@ -2,44 +2,105 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-interface Pattern {
-  start: string;
-  end: string;
-}
-
 /**
- * Collection of comment patterns referenced throughout the extension.
+ * Cycles through all single-line comment types for each selected comment
  */
-const patterns = {
-  singleLine: [
-    { start: '//', end: '\n' },
+function handleCycleSingleLineStyles() {
+  /** All possible patterns for single-line comments. */
+  const commentPatterns = [
+    { start: '//', end: '' },
     { start: '/**', end: '*/' }, // Has to come before single star or it messes up the replacement!
     { start: '/*', end: '*/' },
-  ] as Pattern[],
-};
+  ];
 
-/**
- * Determines whether the given line is a comment line.
- */
-function textMatchesPattern(
-  pattern: keyof typeof patterns,
-  text: string
-): [boolean, number] {
-  const patternList = patterns[pattern];
+  function textMatchesPattern(text: string): [boolean, number] {
+    let matchingPatternIndex = -1;
 
+    const textMatchesPattern = commentPatterns.some((pattern, patternIndex) => {
+      const start = text.substring(0, pattern.start.length);
+      const end = text.substring(text.length - pattern.end.length);
+      const patternMatches =
+        start === pattern.start && (end === pattern.end || pattern.end === '');
+      if (patternMatches) {
+        matchingPatternIndex = patternIndex;
+      }
+      return patternMatches;
+    });
+    return [textMatchesPattern, matchingPatternIndex];
+  }
+
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+
+  /** All the instances of selected text. */
+  const selections = editor.selections;
+
+  /**
+   * Index of the `patterns` array that matches in the selections that are comments.
+   * This is used to increment to the next pattern type.
+   */
   let matchingPatternIndex = -1;
 
-  const textMatchesPattern = patternList.some((pattern, patternIndex) => {
-    const start = text.substring(0, pattern.start.length);
-    const end = text.substring(text.length - pattern.end.length);
-    const patternMatches =
-      start === pattern.start && (end === pattern.end || pattern.end === '\n');
-    if (patternMatches) {
+  /**
+   * Remove selections that
+   *   1. don't match any `patterns`
+   *   2. are multi-line comments
+   */
+  const relevantSelections = selections.filter((selection) => {
+    const text = editor.document.getText(selection);
+
+    // TODO We should automatically cycle through multi-line styles automatically if detected
+    if (text.includes('\n')) {
+      return false;
+    }
+
+    const [textMatches, patternIndex] = textMatchesPattern(text);
+    // If the text matches a pattern, set the matchingPatternIndex to the index of the pattern
+    if (textMatches) {
       matchingPatternIndex = patternIndex;
     }
-    return patternMatches;
+    return textMatches;
   });
-  return [textMatchesPattern, matchingPatternIndex];
+
+  let nextPatternIndex = matchingPatternIndex + 1;
+  const nextPattern =
+    commentPatterns[
+      nextPatternIndex === commentPatterns.length ? 0 : nextPatternIndex
+    ];
+
+  /** Apply next pattern to all matching selections. */
+  const patterns = [
+    { start: '//', end: '' },
+    { start: '/**', end: '*/' }, // Has to come before single star or it messes up the replacement!
+    { start: '/*', end: '*/' },
+  ];
+
+  // Replace all matching selections with the next pattern.
+  editor.edit((editBuilder) => {
+    relevantSelections.forEach((selection) => {
+      const text = editor.document.getText(selection);
+      const [textMatches, matchingPatternIndex] = textMatchesPattern(text);
+
+      if (!textMatches || matchingPatternIndex === -1) {
+        return;
+      }
+
+      const matchingPattern = patterns[matchingPatternIndex];
+      // Replace comment at start of line with next pattern.
+      let newText = text.replace(matchingPattern.start, nextPattern.start);
+      // Replace pattern at end of line with next pattern
+      // Double-slash comment has no end pattern, so we don't replace it.
+      if (matchingPattern.end === '') {
+        newText = `${newText}${nextPattern.end}`;
+      } else {
+        newText.replace(matchingPattern.end, nextPattern.end);
+      }
+
+      editBuilder.replace(selection, newText);
+    });
+  });
 }
 
 // this method is called when your extension is activated
@@ -54,71 +115,7 @@ export function activate(context: vscode.ExtensionContext) {
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'cycle-comment-styles.cycleSingleLineStyles',
-    () => {
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        return;
-      }
-
-      const selections = editor.selections;
-
-      /**
-       * Index of the `patterns` array that matched.
-       * This is used to increment to the next pattern type.
-       */
-      let matchingPatternIndex = -1;
-
-      /** Remove selections that don't match any `patterns` */
-      const selectionsWithComments = selections.filter((selection, index) => {
-        const text = editor.document.getText(selection);
-        const [textMatches, patternIndex] = textMatchesPattern(
-          'singleLine',
-          text
-        );
-        if (textMatches) {
-          matchingPatternIndex = patternIndex;
-        }
-        return textMatches;
-      });
-
-      let nextPatternIndex = matchingPatternIndex + 1;
-      const nextPattern =
-        patterns.singleLine[
-          nextPatternIndex === patterns.singleLine.length ? 0 : nextPatternIndex
-        ];
-
-      /** Apply next pattern to all matching selections. */
-
-      editor.edit((editBuilder) => {
-        selectionsWithComments.forEach((selection) => {
-          const text = editor.document.getText(selection);
-
-          const [textMatches, patternIndex] = textMatchesPattern(
-            'singleLine',
-            text
-          );
-
-          if (!textMatches || patternIndex === -1) {
-            return;
-          }
-
-          const matchingPattern = patterns.singleLine[patternIndex];
-          let newText = text.replace(matchingPattern.start, nextPattern.start);
-
-          if (matchingPattern.end !== '\n') {
-            newText = newText.replace(
-              matchingPattern.end,
-              nextPattern.end !== '\n' ? nextPattern.end : ''
-            );
-          } else {
-            newText = `${newText} ${
-              nextPattern.end !== '\n' ? nextPattern.end : ''
-            }`;
-          }
-          editBuilder.replace(selection, newText);
-        });
-      });
-    }
+    () => handleCycleSingleLineStyles()
   );
 
   context.subscriptions.push(disposable);
